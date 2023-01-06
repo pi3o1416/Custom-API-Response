0c09bbdc1c8adf4db14ff3caa1ae9ee1c246e21e
Monir Hossain <mmonir.m.29@gmail.com>
2023-01-06 23:41:42 +0600

    :sparkles: Add new app book.

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd party apps
+    'rest_framework',
+
+    #Local Apps
+    'book.apps.BookConfig',
 ]
 
 MIDDLEWARE = [
