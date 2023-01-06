3360e7aa3d469905a86d3c6f34640bf5d332354f
Monir Hossain <mmonir.m.29@gmail.com>
2023-01-07 00:24:51 +0600

    Create AuthorViewSet with author list method.

--- author/views.py
+++ author/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+
+from rest_framework import status
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from .models import Author
+from .serializers import AuthorSerializer
 
 # Create your views here.
+
+class AuthorViewSet(ViewSet, PageNumberPagination):
+    def list(self, request):
+        authors = Author.objects.all()
+        page = self.paginate_queryset(queryset=authors, request=request)
+        serializer = AuthorSerializer(instance=page, many=True)
+        return self.get_paginated_response(data=serializer.data)
