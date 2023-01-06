096ce1ebded6134ba3f242fc1dfbee0e4a79e2c7
Monir Hossain <mmonir.m.29@gmail.com>
2023-01-07 00:03:39 +0600

    :sparkles: Add new Table Author.

--- author/models.py
+++ author/models.py
@@ -1,3 +1,16 @@
+
+from django.utils.translation import gettext_lazy as _
 from django.db import models
+from django_countries.fields import CountryField
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(
+        verbose_name=_("Author Name"),
+        max_length=100,
+    )
+    date_of_birth = models.DateField(
+        verbose_name=_("Author Date Of Birth"),
+    )
+    country = CountryField()
